c16e5152650531ef638b30176c9e105f3eb3a061
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-17 11:18:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // index.js
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
