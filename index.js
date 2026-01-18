a0f02a832016140c550643ed2c6f7890a23dad01
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-18 05:18:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import tradeRoutes from "./routes/trade.routes.js";
-import walletRoutes from "./routes/wallet.routes.js"; // if exists
-
 // Load environment variables first
 dotenv.config();
 
