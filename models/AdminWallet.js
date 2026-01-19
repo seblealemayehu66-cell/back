b8da81ce2715e492f6f8e475691ffe301eddab16
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-19 16:04:58 -0500

    Update and rename Wallet.js to AdminWallet.js

---
+++ models/AdminWallet.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const adminWalletSchema = new mongoose.Schema({
+  coin: { type: String, required: true, uppercase: true },
+  network: { type: String, required: true },
+  address: { type: String, required: true, unique: true },
+  active: { type: Boolean, default: true }
+}, { timestamps: true });
+
+export default mongoose.model("AdminWallet", adminWalletSchema);
