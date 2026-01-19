62067320e50627d359dd88d1645215048f6d08b2
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-19 12:12:32 -0500

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -26,7 +26,7 @@ router.post("/register", async (req, res) => {
       username,
       email,
       password: hashed,
-      balance, // demo balance
+    
     });
 
     await Notification.create({
