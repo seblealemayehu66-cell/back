917515378f6e4c09b86f17e2348ad6207c226a50
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-21 10:28:41 -0500

    Update Settings.js

--- models/Settings.js
+++ models/Settings.js
@@ -1,9 +1,9 @@
-// models/Settings.js
 import mongoose from "mongoose";
 
-const settingsSchema = new mongoose.Schema({
-  tradingOpen: { type: Boolean, default: true },
-  profitPercent: { type: Number, default: 30 }
-});
+export default mongoose.model(
+  "Settings",
+  new mongoose.Schema({
+    tradingOpen: { type: Boolean, default: true }
+  })
+);
 
-export default mongoose.model("Settings", settingsSchema);
