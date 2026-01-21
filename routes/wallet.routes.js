ac2dfeb927a8dae35bc98c86372398618788fc56
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-21 16:33:31 -0500

    Update wallet.routes.js

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -1,43 +1,16 @@
 import express from "express";
-import User from "../models/User.js";
 import Wallet from "../models/Wallet.js";
-import auth from "../middleware/auth.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", auth, async (req, res) => {
-  try {
-    // USDT from user model
-    const user = await User.findById(req.userId).select("balance");
+/**
+ * USER FETCH WALLETS AFTER LOGIN
+ */
+router.get("/", authMiddleware, async (req, res) => {
+  const wallets = await Wallet.find({ status: true });
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // coins from wallet model
-    const wallets = await Wallet.find({ userId: req.userId });
-
-    const coins = wallets.map((w) => ({
-      coin: w.coin,
-      symbol: w.symbol,
-      balance: w.balance || 0,
-    }));
-
-    // final wallet response
-    const data = [
-      {
-        coin: "Tether",
-        symbol: "USDT",
-        balance: user.balance,
-      },
-      ...coins,
-    ];
-
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Wallet fetch failed" });
-  }
+  res.json(wallets);
 });
 
 export default router;
