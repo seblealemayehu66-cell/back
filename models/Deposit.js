95972f4f6f96af3523edb25c6c38a840563b65c5
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-21 08:49:43 -0500

    Deposit.js

---
+++ models/Deposit.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const depositSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    coin: String,
+    network: String,
+    txid: String,
+
+    amount: Number,
+
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending"
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Deposit", depositSchema);
