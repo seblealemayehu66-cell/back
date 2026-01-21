c6656856bd7d8dcb789706d56b882340f0879cd7
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-21 12:21:12 -0500

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-export default mongoose.model(
-  "Admin",
-  new mongoose.Schema({
-    email: String,
-    password: String
-  })
-);
+const adminSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
+// Hash password before save
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+adminSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("Admin", adminSchema);
