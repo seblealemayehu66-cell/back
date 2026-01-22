b37b05d00d0dd36921b0bc4dc553db0c5aaebff7
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-22 10:09:06 -0500

    Update wallet.routes.js

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -19,9 +19,41 @@ router.get("/wallets", auth, async (req, res) => {
     console.error(err);
     res.status(500).json({ message: "Server error" });
   }
+  
+});
+router.post("/wallets/add-balance", auth, async (req, res) => {
+  try {
+    const { symbol, amount } = req.body;
+
+    if (!symbol || !amount) {
+      return res.status(400).json({ message: "Missing data" });
+    }
+
+    const wallet = await Wallet.findOne({
+      userId: req.user._id,
+      symbol
+    });
+
+    if (!wallet) {
+      return res.status(404).json({ message: "Wallet not found" });
+    }
+
+    wallet.balance += Number(amount);
+    await wallet.save();
+
+    res.json({
+      message: "Balance added",
+      wallet
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 export default router;
+
 
 
 
