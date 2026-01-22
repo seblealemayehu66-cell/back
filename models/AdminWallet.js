60fb9123ea377cc08029ecfda4f326702919b83f
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-22 13:33:20 -0500

    Update AdminWallet.js

--- models/AdminWallet.js
+++ models/AdminWallet.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const adminWalletSchema = new mongoose.Schema({
-  coin: { type: String, required: true },
-  network: { type: String, required: true },
-  address: { type: String, required: true, unique: true },
-  active: { type: Boolean, default: true },
-}, { timestamps: true });
+  coin: String,
+  symbol: String,
+  network: String,
+  address: String,
+  qrCode: String
+});
 
 export default mongoose.model("AdminWallet", adminWalletSchema);
+
