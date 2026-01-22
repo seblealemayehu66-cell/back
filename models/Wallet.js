436ba81f7d0d94c94acdcea623dd1b6d00706727
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-22 09:08:05 -0500

    Update Wallet.js

--- models/Wallet.js
+++ models/Wallet.js
@@ -2,29 +2,23 @@ import mongoose from "mongoose";
 
 const walletSchema = new mongoose.Schema(
   {
-    coin: {
-      type: String,
-      required: true,
-      uppercase: true
-    },
-
-    network: {
-      type: String,
-      required: true,
-      uppercase: true
-    },
-
-    address: {
-      type: String,
-      required: true
-    },
-
-    status: {
-      type: Boolean,
-      default: true
-    }
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
   },
-  { timestamps: true }
-);
+
+  coin: String,
+  symbol: String,
+
+  balance: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
 
 export default mongoose.model("Wallet", walletSchema);
+
+
+
+  
