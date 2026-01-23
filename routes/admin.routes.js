d35e643d5de8d11c27d434a71f240c3020a7d16c
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-23 08:05:42 -0500

    Update admin.routes.js

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import { adminAuth } from "../middleware/adminAuth.js"; // Protect admin routes
+import  adminAuth  from "../middleware/adminAuth.js"; // Protect admin routes
 
 const router = express.Router();
 
