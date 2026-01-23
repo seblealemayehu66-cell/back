c3f79e6333b9e84bd909192c04aa281e56737e96
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-01-23 04:02:43 -0500

    Update createAdmin.js

--- createAdmin.js
+++ createAdmin.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import Admin from "./models/Admin.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 mongoose.connect(process.env.MONGO_URI);
 
